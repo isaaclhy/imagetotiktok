f69053784412f2536a32f783cf4983a8fb459bc8
isaaclhy <leehuanyoei2025@gmail.com>
2026-03-01 21:38:41 +0000

    fixed

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -4,21 +4,24 @@ import { QuestionVideo, QuestionVideoProps } from './QuestionVideo';
 
 const FPS = 30;
 
+const QuestionVideoComp = QuestionVideo as React.FC<Record<string, unknown>>;
+
 export const RemotionRoot: React.FC = () => {
-  const defaultProps = { title: 'Sample Title', questions: ['Question 1', 'Question 2', 'Question 3'] };
+  const defaultProps: QuestionVideoProps = { title: 'Sample Title', questions: ['Question 1', 'Question 2', 'Question 3'] };
 
   return (
     <Composition
       id="QuestionVideo"
-      component={QuestionVideo}
+      component={QuestionVideoComp}
       durationInFrames={FPS * 5 + (defaultProps.questions.length + 1) * FPS * 10}
       fps={FPS}
       width={1080}
       height={1920}
-      defaultProps={defaultProps}
-      calculateMetadata={({ props }: { props: QuestionVideoProps }) => ({
-        durationInFrames: FPS * 5 + (props.questions.length + 1) * FPS * 10,
-      })}
+      defaultProps={defaultProps as Record<string, unknown>}
+      calculateMetadata={({ props }) => {
+        const p = props as unknown as QuestionVideoProps;
+        return { durationInFrames: FPS * 5 + (p.questions.length + 1) * FPS * 10 };
+      }}
     />
   );
 };
